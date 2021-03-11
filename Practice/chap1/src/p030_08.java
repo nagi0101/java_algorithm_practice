78c36a3b52e4229b3289165dbf9e4ce7207d2354
Jiin Park <demon0315@naver.com>
2021-03-11 18:11:29 +0900

    chap01 Practice p030_08

--- Practice/chap1/src/p030_08.java
+++ Practice/chap1/src/p030_08.java
@@ -5,31 +5,45 @@ public class p030_08 {
         int sum = 0;
         Scanner stdIn = new Scanner(System.in);
 
-        System.out.println("bottom부터 top까지의 합을 출력합니다.");
+        System.out.println("1부터 top까지의 합을 출력합니다.");
 
-        System.out.print("bottom : ");
-        int bottom = stdIn.nextInt();
+        // System.out.print("bottom : ");
+        // int bottom = stdIn.nextInt();
 
         System.out.print("top : ");
         int top = stdIn.nextInt();
 
-        sum = gaussPlus(bottom, top);
+        sum = gaussPlus(top);
 
-        System.out.println(bottom + "부터 " + top + "까지의 합은 " + sum + "입니다.");
+        System.out.println(1 + "부터 " + top + "까지의 합은 " + sum + "입니다.");
 
     }
 
+
+    // gaussPlus for add {bottom} to {top}
     static int gaussPlus(int bottom, int top) {
-        int distance = top - bottom + 1;
-        int n = distance / 2;
-        int sum = (bottom + top) * n;
+        int distance = top - bottom + 1; // Count of numbers we have to calculate
+        int sum = (bottom + top) * (distance / 2);
 
         if (distance % 2 == 1) {
-            // 중간의 하나의 숫자에게 쌍이 없는 경우
-            // 가운데 숫자를 구해서 더해준다
+            // If (median number has no pair) => add median number to sum.
             sum += (bottom + top) / 2;
         }
 
         return sum;
     }
+
+    // gaussPlus for add 1 to {top}
+    static int gaussPlus(int top) {
+        int distance = top; // Count of numbers we have to calculate
+        int sum = (1 + top) * (distance / 2);
+
+        if (distance % 2 == 1) {
+            // If (median number has no pair) => add median number to sum.
+            sum += (1 + top) / 2;
+        }
+
+        return sum;
+    }
+
 }
