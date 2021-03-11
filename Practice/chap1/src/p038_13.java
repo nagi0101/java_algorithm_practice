589fb2a992c6790b7873dc525c6455339448af7f
Jiin Park <demon0315@naver.com>
2021-03-11 20:01:14 +0900

    chap01 Practice p038_13

---
+++ Practice/chap1/src/p038_13.java
@@ -0,0 +1,25 @@
+public class p038_13 {
+    public static void main(String[] args) {
+        printHeader();
+        for (int i = 1; i <= 9; i++) printLine(i);
+    }
+
+    static void printHeader() {
+        System.out.print("  |");
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%3d", i);
+        }
+        System.out.println();
+        System.out.print("--+");
+        for (int i = 0; i < 9; i++) System.out.print("---");
+        System.out.println();
+    }
+
+    static void printLine(int n) {
+        System.out.printf("%-2d|", n);
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%3d", i + n);
+        }
+        System.out.println();
+    }
+}
