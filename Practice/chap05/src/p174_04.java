45201584b58be629fd346651b0d140580f3ac887
Jiin Park <demon0315@naver.com>
2021-03-28 18:24:56 +0900

    chap05 Practice p174_04

---
+++ Practice/chap05/src/p174_04.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class p174_04 {
+    static void recur2(int n){
+        if(n > 0){
+            recur2(n-2);
+            System.out.println(n);
+            recur2(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("Input integer : ");
+        int x = stdIn.nextInt();
+
+        recur2(x);
+    }
+}
