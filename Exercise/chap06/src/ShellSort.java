58326201a126b0c08eb492d750a7b1ddb61232b6
Jiin Park <demon0315@naver.com>
2021-04-02 15:11:48 +0900

    chap06 Exercise ShellSort

---
+++ Exercise/chap06/src/ShellSort.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ShellSort {
+    static void shellSort(int[] a, int n) {
+        for (int h = n / 2; h > 0; h /= 2)
+            for (int i = h; i < n; i++) {
+                int j;
+                int tmp = a[i];
+                for (j = i - h; j >= 0 && a[j] > tmp; j -= h)
+                    a[j + h] = a[j];
+                a[j + h] = tmp;
+            }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("Shell Sort(v1)");
+        System.out.print("Number of items : ");
+        int nx = stdIn.nextInt();
+        int[] x = new int[nx];
+
+        for (int i = 0; i < nx; i++) {
+            System.out.print("x[" + i + "] : ");
+            x[i] = stdIn.nextInt();
+        }
+
+        shellSort(x, nx);
+
+        System.out.println("Ascending ordered.");
+        for (int i = 0; i < nx; i++)
+            System.out.println("x[" + i + "] = " + x[i]);
+    }
+}
